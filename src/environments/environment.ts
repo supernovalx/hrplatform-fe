6cb1dbc96c4994e516b420e246719ffe383d4149
supernovalx <hoanganhgo9@gmail.com>
2020-05-19 10:15:18 +0700

    add firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyDtC-c2wbGFIHww7u-sAU5XM5xIcxMjqy8",
+    authDomain: "hrp-test-799f4.firebaseapp.com",
+    databaseURL: "https://hrp-test-799f4.firebaseio.com",
+    projectId: "hrp-test-799f4",
+    storageBucket: "hrp-test-799f4.appspot.com",
+    messagingSenderId: "323222663860",
+    appId: "1:323222663860:web:0ec7f9a7ce6c4a8f18dde1",
+    measurementId: "G-PE8VSJ653Q"
+  }
 };
 
 /*
