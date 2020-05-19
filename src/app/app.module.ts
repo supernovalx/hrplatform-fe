b26e2a6472268e883bd32db25de078cea9b8da09
SuperNovalx <hoanganhgo9@hotmail.com>
2020-05-19 11:12:00 +0700

    add firebase bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,18 +10,32 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { Ng2GoogleChartsModule, GoogleChartsSettings } from 'ng2-google-charts';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// Firebase services + enviorment module
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DepartmentChartComponent
+    DepartmentChartComponent,
+    SignInComponent,
+    SignUpComponent,
+    ForgotPasswordComponent
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     HttpClientModule,
     Ng2GoogleChartsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
